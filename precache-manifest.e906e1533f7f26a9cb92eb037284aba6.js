5a67316b375a6ab6cf13c24818cacc237a122f68
aniket <aniketuu42@gmail.com>
2020-03-09 12:30:26 +0530

    Updates

---
+++ precache-manifest.e906e1533f7f26a9cb92eb037284aba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c1673ad386315cb9fdb34f1d6a7291",
+    "url": "/Flix-data-app/index.html"
+  },
+  {
+    "revision": "2bf0916cacb092bf8f94",
+    "url": "/Flix-data-app/static/css/main.d20d7e65.chunk.css"
+  },
+  {
+    "revision": "b722d94097bfd8879a51",
+    "url": "/Flix-data-app/static/js/2.c59e2212.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Flix-data-app/static/js/2.c59e2212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf0916cacb092bf8f94",
+    "url": "/Flix-data-app/static/js/main.3aae7028.chunk.js"
+  },
+  {
+    "revision": "aba72c92ed3d47234959",
+    "url": "/Flix-data-app/static/js/runtime-main.a216b6ed.js"
+  }
+]);
